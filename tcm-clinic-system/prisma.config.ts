d16d8925e29872f1afb613254e77f0ab4ae3afd8
mrktp <misterkantapat@gmail.com>
2026-02-22 00:27:04 +0700

    update schema

--- tcm-clinic-system/prisma.config.ts
+++ tcm-clinic-system/prisma.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@prisma/config";
 import "dotenv/config";
 
+// $env:DATABASE_URL="your_actual_connection_string_here"; npx prisma db pull
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
